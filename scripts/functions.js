91c6cfce3dab977387299457833be3b00c05f375
Arian Parvin <arian.parvin@parvin.de>
2019-04-02 16:53:21 +0200

    random Char proto

--- scripts/functions.js
+++ scripts/functions.js
@@ -11,23 +11,23 @@ const startMorseMouse = () => {
 	var elem = document.querySelector(".container")
 	elem.remove()
 
-	/* var outputFieldVar = document.createElement("div")
-     document.body.appendChild(gameBoardVar)  */
+	var randomChars = function(obj) {
+		var chars = charLib.keys(obj)
+		return obj[keys[(chars.length * Math.random()) << 0]]
+	}
 
 	var gameBoardVar = document.createElement("div")
 	gameBoardVar.innerHTML = "CLICK ME"
-	gameBoardVar.style.height = "800px"
-	gameBoardVar.style.width = "1000px"
+	gameBoardVar.style.height = "500px"
+	gameBoardVar.style.width = "500px"
 	gameBoardVar.style.backgroundColor = "papayawhip"
 	gameBoardVar.classList.add("gameBoard")
 
 	document.body.appendChild(gameBoardVar)
 
-	var node = document.getElementsByClassName("gameBoard").lastChild
-	document.getElementById("myList1").appendChild(node)
-
-	var inputFieldVar = document.createElement("div")
-	document.body.appendChild(gameBoardVar)
+	var randomGen = document.createElement("div")
+	randomGen.classList.add("randomGen")
+	randomGen.innerHTML = randomChars()
 }
 
 // const noEmpties = function(arr) {
@@ -52,10 +52,14 @@ var dash = new Audio("../assets/samples/dash.mp3")
 dash.load()
 var wb1 = new Audio("../assets/samples/Woodblock 1 copy.wav")
 wb1.load()
+var wb2 = new Audio("../assets/samples/Woodblock 3 copy.wav")
+wb2.load()
 var snr = new Audio("../assets/samples/Snare 10 copy.wav")
 snr.load()
 var bdr = new Audio("../assets/samples/Kick 17.wav")
 bdr.load()
+var hats = new Audio("../assets/samples/Hihat 22 copy.wav")
+hats.load()
 
 const addDit = function() {
 	var dit = document.createElement("div")
--- scripts/input.js
+++ scripts/input.js
@@ -51,13 +51,13 @@ let assignChar = str => {
 Mousetrap.bind("o", function() {
 	console.log("dit")
 	tempStr.push("o")
-	period.play()
+	wb1.play()
 }) //Binds i to dit and pushes 0 to tempStr
 
 Mousetrap.bind("i", function() {
 	console.log("dah")
 	tempStr.push("i")
-	dash.play()
+	wb2.play()
 }) //Binds o to dah and pushes 1 to tempStr
 
 Mousetrap.bind("space", function() {
@@ -65,12 +65,12 @@ Mousetrap.bind("space", function() {
 	assignChar(tempStr.join(""))
 	document.getElementById("output").innerText = conStr.join("")
 	tempStr = []
-	wb1.play()
+	snr.play()
 }) //Binds space to nextChar
 
 Mousetrap.bind("s", function() {
 	conStr.push(" ")
-	snr.play()
+	hats.play()
 })
 //binds s to newWord (no empties)
 Mousetrap.bind("backspace", function() {
